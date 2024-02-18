bdfe2da2355b220a913ca00ffb63d7cfbaf278bc
Vincent Mukuna <mukunavincent28@gmail.com>
2024-02-18 23:05:26 +0300

    [refactor] add rule

---
+++ app/Rules/ChirpExists.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Chirp;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class ChirpExists implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if(!Chirp::find($value)->exists()){
+            $fail('The original chirp does not exist. It may have been deleted');
+        }
+    }
+
+}
