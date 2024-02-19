b166fb49c43d2077949cfa70c67b236d9ef97675
Vincent Mukuna <mukunavincent28@gmail.com>
2024-02-19 20:53:04 +0300

    [test] delete chirp tests
    . Only creator can delete chirp

--- tests/Feature/Chirp/ChirpActionsTest.php
+++ tests/Feature/Chirp/ChirpActionsTest.php
@@ -3,8 +3,8 @@
 namespace Tests\Feature\Chirp;
 use App\Models\Chirp;
 use App\Models\User;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
 class ChirpActionsTest extends TestCase
 {
@@ -16,18 +16,43 @@ class ChirpActionsTest extends TestCase
             'user_id'=>$user->id,
         ]);
 
-        $response = $this
-            ->actingAs($user)
-            ->fromRoute('chirps.index')
-            ->delete(route('chirps.destroy',[
-                'chirp'=>$chirp->id,
-            ]));
+        $response = $this->deleteChirp($user, $chirp);
 
         $response
             ->assertSessionHasNoErrors()
             ->assertRedirect(route('chirps.index'));
 
-        $this->assertNull($chirp->fresh());
+        $this->assertDatabaseMissing(Chirp::class, [
+            'id'=>$chirp->id,
+        ]);
+    }
+
+    private function deleteChirp(User $user, Chirp $chirp): TestResponse
+    {
+        return $this
+            ->actingAs($user)
+            ->fromRoute('chirps.index')
+            ->delete(route('chirps.destroy',[
+                'chirp'=>$chirp->id,
+            ]));
+    }
+
+    public function test_other_users_cant_delete_chirp():void
+    {
+        $chirper = User::factory()->create();
+        $chirp = Chirp::factory()->create([
+            'user_id'=>$chirper->id,
+        ]);
+
+        $otherUser = User::factory()->create();
+
+        $response = $this->deleteChirp($otherUser, $chirp);
+
+        $response
+            ->assertStatus(403);
+
+        $this->assertTrue($chirper->refresh()->chirps()->count()===1);
+
     }
 
 }
