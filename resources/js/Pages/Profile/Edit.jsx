92b747599fbb33e04cb4a16aea7bcb7c03e681d7
Vincent Mukuna <mukunavincent28@gmail.com>
2024-02-21 19:31:18 +0300

    [feat] add link to chirper profile

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -2,13 +2,21 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import DeleteUserForm from './Partials/DeleteUserForm';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
-import { Head } from '@inertiajs/react';
+import {Head, router} from '@inertiajs/react';
+import SecondaryButton from "@/Components/SecondaryButton.jsx";
 
 export default function Edit({ auth, mustVerifyEmail, status }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Profile</h2>}
+            header={
+            <h2
+                className="font-semibold flex justify-between text-xl text-gray-800 leading-tight"
+            >
+                Account
+                <SecondaryButton onClick={()=>router.get(route('users.show', [auth.user.id]))}>Chirper Profile</SecondaryButton>
+            </h2>
+        }
         >
             <Head title="Profile" />
 
