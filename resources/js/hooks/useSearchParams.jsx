7b8073593388f03f5122b9e63e55b2514661156f
Vincent Mukuna <mukunavincent28@gmail.com>
2024-02-26 02:07:50 +0300

    [fix] empty search params list

--- resources/js/hooks/useSearchParams.jsx
+++ resources/js/hooks/useSearchParams.jsx
@@ -3,9 +3,9 @@ import {usePage} from "@inertiajs/react";
 export default function useSearchParams(){
     const url = usePage().url
     const searchStr = url.split('?').at(1)
-    const params = searchStr.split('&').map(str=>{
+    const params = searchStr?searchStr.split('&').map(str=>{
         return str.split('=');
-    })
+    }):[];
 
     return new Map(params);
 }
